13c1b5184754030eb3d0062fa320d4c00de87247
oac5kor <PoojaJayant.Kanchugar@in.bosch.com>
2021-04-25 14:09:16 +0530

    added install dependencies script

---
+++ install_dependencies.sh
@@ -0,0 +1,19 @@
+rm -rf venv/
+
+# -------install python 3.6-------------
+sudo apt-get install python3.6
+python3.6 -m venv venv
+. venv/bin/activate
+
+# ------------install mongodb-community edition----------------
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt-get install -y mongodb-org
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+
+# ------------install python packages required--------------
+pip install --no-cache-dir -r requirements.txt
